6d892d0a13abe5e436cfac98cd9f6ff930cdc325
Jonathan Ford <jford@inogs.it>
2022-06-07 14:51:27 +0200

    modified centroid work

--- pycurious/optimise_bansal.py
+++ pycurious/optimise_bansal.py
@@ -13,7 +13,19 @@
 """
 
 from .optimise_tanaka import CurieOptimiseTanaka
+import numpy as np
+import warnings
+from scipy.optimize import curve_fit
 
 class CurieOptimiseBansal(CurieOptimiseTanaka):
     def __init__(self, grid, x_0, x_1, y_0, y_1, **kwargs):
         super(CurieOptimiseBansal, self).__init__(grid, x_0, x_1, y_0, y_1)
+
+    def calculate_spectra(self, subgrid, taper, beta=2.0):
+        k, Phi, sigma_Phi = self.radial_spectrum(subgrid, taper=taper, power=1)
+
+        Phi
+        Phi_n = np.log(np.exp(Phi) / k)
+        sigma_Phi_n = np.log(np.exp(sigma_Phi) / k)
+
+        return k, Phi, Phi_n, sigma_Phi, sigma_Phi_n
